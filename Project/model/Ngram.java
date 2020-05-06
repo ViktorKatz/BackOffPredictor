72cdb709bf5beb5ca729fc983716ea37dde93064
ViktorKatz <viktorkatz619@gmail.com>
2020-05-06 13:00:40 +0200

    OK, napravio sam projekat.

---
+++ Project/model/Ngram.java
@@ -0,0 +1,18 @@
+package model;
+
+import java.io.Serializable;
+
+public class Ngram implements Serializable {
+
+	/**
+	 * Mora imati verziju za serijalizaciju da java ne bi lupila svoju verziju.
+	 * Ako su razlicite verzije, nekad baca gresku.
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public static void main(String[] args) {
+		System.out.print("Ovde idu testiranja");
+
+	}
+
+}
