dd4fb83f0921d55ec094e79c90c5ae65b2664b88
ViktorKatz <viktorkatz619@gmail.com>
2020-05-07 13:07:35 +0200

    Sredio klasu FollowingWord u Ngramu

--- Project/model/Ngram.java
+++ Project/model/Ngram.java
@@ -1,17 +1,44 @@
 package model;
 
 import java.io.Serializable;
+import java.security.InvalidParameterException;
+import java.util.PriorityQueue;
 
 public class Ngram implements Serializable {
 
 	protected class FollowingWord implements Comparable<FollowingWord>{
-		String word;
-		int timesAppeared = 1;
+		
+		protected final String word;
+		protected int timesAppeared = 1;
+
+		public FollowingWord(String word) {
+			this.word=word;
+		}
+		
+		/**
+		 * @comment Mislim da nam nece trebati.
+		 * @param timesAppeared Ako se rec vec pojavljivala vise puta. 
+		 */
+		public FollowingWord(String word, int timesAppeared) {
+			this(word);
+			this.timesAppeared=timesAppeared;
+		}
+		
+		public String getWord() {
+			return word;
+		}
+		
+		public int getTimesAppeared() {
+			return timesAppeared;
+		}
+		
+		public void increaseTimesAppeared() {
+			timesAppeared++;
+		}
 		
 		@Override
 		public int compareTo(FollowingWord arg0) {
-			// TODO Auto-generated method stub
-			return 0;
+			return this.timesAppeared-arg0.timesAppeared;
 		}
 		
 	}
@@ -21,6 +48,19 @@ public class Ngram implements Serializable {
 	 * Ako su razlicite verzije, nekad baca gresku.
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	protected final int N;
+	protected final String[] words;
+	protected final PriorityQueue<FollowingWord> predictions;
+	
+	public Ngram(int N, String[] words) {
+		if (words.length != N)
+			throw new InvalidParameterException("Ngram nema odgovarajuci broj reci."); 
+		
+		this.N=N;
+		this.words=words;
+		this.predictions=new PriorityQueue<Ngram.FollowingWord>();
+	}
 
 	public static void main(String[] args) {
 		System.out.print("Testing testing 123");
