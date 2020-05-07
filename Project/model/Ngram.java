96d7b42f026abf2a143287643f25b7089c5668d1
ViktorKatz <viktorkatz619@gmail.com>
2020-05-07 12:11:05 +0200

    Dodao unutrasnju klasu FollowingWord u Ngram i dodao par funkcija nasoj semi.

--- Project/model/Ngram.java
+++ Project/model/Ngram.java
@@ -4,6 +4,18 @@ import java.io.Serializable;
 
 public class Ngram implements Serializable {
 
+	protected class FollowingWord implements Comparable<FollowingWord>{
+		String word;
+		int timesAppeared = 1;
+		
+		@Override
+		public int compareTo(FollowingWord arg0) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+		
+	}
+	
 	/**
 	 * Mora imati verziju za serijalizaciju da java ne bi lupila svoju verziju.
 	 * Ako su razlicite verzije, nekad baca gresku.
